dd04fa35183b28fd2d101346654b781d81a058c4
corbant <corbant25@gmail.com>
2022-01-07 00:03:39 -0800

    It works!!!!

--- find_homography.py
+++ find_homography.py
@@ -20,7 +20,7 @@ world_bottom_right = (13.5, -7, 1)
 
 #homography_matrix = cv.findHomography(src_pts, dst_pts)
 
-duck_image_pos = np.array([[635], [330], [1]])
+duck_image_pos = np.array([[1090], [420], [1]])
 
 #calculated_duck_pos = np.matmul(homography_matrix[0], duck_image_pos)
 
@@ -71,7 +71,7 @@ for image in images:
 
         # Draw and display the corners
         cv.drawChessboardCorners(img, chessboardSize, corners2, ret)
-        cv.imshow('img', img)
+        # cv.imshow('img', img)
         #cv.waitKey(1000)
 
 
@@ -83,11 +83,6 @@ cv.destroyAllWindows()
 ############## CALIBRATION #######################################################
 
 ret, cameraMatrix, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, frameSize, None, None)
-print("ret: ", ret )
-print("camera matrix: ", cameraMatrix)
-print("dist: ",  dist)
-print("rvecs: ",  rvecs)
-print("tvecs: ",  tvecs)
 
 # img = cv.imread('WIN_20220106_15_19_41_Pro.jpg')
 # h,  w = img.shape[:2]
@@ -97,42 +92,37 @@ criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 objp = np.zeros((9*6,3), np.float32)
 objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+objp = objp * size_of_chessboard_squares_mm
 
 image = "WIN_20220106_17_58_12_Pro.jpg"
 img = cv.imread(image)
 gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 ret, corners = cv.findChessboardCorners(gray, (9,6),None)
 
-if ret == True:
+corners2 = cv.cornerSubPix(gray,corners,(11,11),(-1,-1), criteria)
 
-    corners2 = cv.cornerSubPix(gray,corners,(11,11),(-1,-1), criteria)
-
-    # Find the rotation and translation vectors.
-    ret, rvecs, tvecs = cv.solvePnP(objp, corners2, cameraMatrix, dist)
-    print("Box rvec: ", rvecs)
-    print("Box tvec: ", tvecs)
-    cv.drawFrameAxes(img, cameraMatrix, dist, rvecs, tvecs, 50)
-    cv.imshow('img', img)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
+# Find the rotation and translation vectors.
+ret, rvecs, tvecs = cv.solvePnP(objp, corners2, cameraMatrix, dist)
+print("Box rvec: ", rvecs)
+print("Box tvec: ", tvecs)
+# cv.drawFrameAxes(img, cameraMatrix, dist, rvecs, tvecs, 100)
+# cv.imshow('img', img)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
 
 rotation_matrix = cv.Rodrigues(rvecs)
 rotation_matrix = rotation_matrix[0]
+print("Rotation matrix: ", rotation_matrix)
+print("Camera matrix: ", cameraMatrix)
 
-Z_CONST = 0
+Z_CONST = 1
 
-temp_mat = inv(rotation_matrix) @ inv(cameraMatrix) @ duck_image_pos
-temp_mat2 = inv(rotation_matrix) @ tvecs
+left_side_mat = inv(rotation_matrix) @ inv(cameraMatrix) @ duck_image_pos
+right_side_mat = inv(rotation_matrix) @ tvecs
 
-s = Z_CONST + temp_mat2[2][0]
-
-s /= temp_mat[2][0]
+s = (Z_CONST + right_side_mat[2][0])/left_side_mat[2][0]
 
 calculated_duck_pos = inv(rotation_matrix) @ ( np.subtract(s * inv(cameraMatrix) @ duck_image_pos, tvecs) )
-#cv.projectPoints(np.array([np.transpose(calculated_duck_pos)]), rvecs, tvecs, cameraMatrix, dist)
-#cv.imshow(img)
-#cv.waitKey(0)
-#cv.destroyAllWindows()
 #calculated_duck_pos = homography_matrix[0] @ duck_image_pos
 #calculated_duck_pos *= (1 / calculated_duck_pos[2][0])
 calculated_duck_pos /= 25.4
